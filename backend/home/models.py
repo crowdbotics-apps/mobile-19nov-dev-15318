e4a62721fcce81ffff36060b9ccbf4e7489b5150
Crowdbotics <team@crowdbotics.com>
2020-11-19 07:31:52 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,75 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
+    fdfgdrg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    wqe = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    qw = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ytu = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    oio = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    oiou = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    wqere = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    yjuy = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    opipuoii = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    iuuioyuy = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    yuogfgs = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +83,16 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
